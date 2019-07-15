1774945fcea3c7b5e0af2eeedde81f96c4569f66
brainbackdoor <brainbackdoor@gmail.com>
2019-07-15 22:48:35 +0900

    fix(ToDoController): Change Package structure

--- src/test/java/todo/demo/web/TodoControllerTest.java
+++ src/test/java/todo/demo/web/TodoControllerTest.java
@@ -1,24 +1,20 @@
-package web;
+package todo.demo.web;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import todo.demo.repository.TodoRepository;
-import todo.demo.web.TodoController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {TodoControllerTest.class, TodoController.class})
-@AutoConfigureMockMvc
+@SpringBootTest
 public class TodoControllerTest {
 
     @Autowired
